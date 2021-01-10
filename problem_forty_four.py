af56697b8253f502492be559ddb654f577477199
Sahabuddin Shakil <mdshakil8796@gmail.com>
2021-01-11 02:54:32 +0600

    tuki taki change forty four and forty threes problem  problems  by py and js

--- problem_forty_four.py
+++ problem_forty_four.py
@@ -1,14 +1,48 @@
 #Pascal's triangle
-inp = int(input("Enter the range:"))
-def MyFunction(inp):
-    lst =["1 1","1 2 1","1 3 3 1","1 4 6 4 1","1 5 10 10 5 1","1 6 15 20 15 6 1","1 7 21 35 35 21 7 1","1 8 28 56 70 56 28 8 1","1 9 36 84 126 126 84 36 9 1",
-    "1 10 45 120 210 252 210 120 45 10 1","1 11 55 165 330 462 462 330 165 55 11 1","1 12 66 220 495 792 924 792 495 220 66 12 1","1 13 78 286 715 1287 1716 1716 1287 715 286 78 13 1",
-    "1 14 91 364 1001 2002 3003 3432 3003 2002 1001 364 91 14 1","1 15 105 455 1365 3003 5005 6435 6435 5005 3003 1365 455 105 15 1"]
-    for i in range(inp):
-        if i== 0:
+inp = int(input("Enter the number:"))
+def MyFunction(inp2):
+    inp =5
+    save_data =[]
+    lst =[]
+    for i in range(inp): 
+        lst2 =[]
+        if i == 0:
             print(1)
-            print(lst[i])
-        elif i >0:    
-            print(lst[i])
-
+            print(1,1) 
+            lst.append(1)
+            lst.append(1)   
+        elif i>0:
+            for j in range(len(lst)-1):
+                if j == 0:
+                    lst2.append(1)
+                    value = lst[j]+lst[j+1]
+                    lst2.append(value)
+                elif j >0:
+                    val =lst[j]+lst[j+1]   
+                    lst2.append(val)  
+            lst2.append(1)
+            save_data.append(lst2) 
+            lst =lst2      
+    if inp2 <= len(save_data)+1:
+        for i in range(inp2-1):
+            listToStr = ' '.join([str(elem) for elem in save_data[i]]) 
+            print(listToStr)         
+    else:
+        for i in range(len(save_data)-1,inp2-2,1):
+            lst2=[] 
+            for j in range(len(save_data[i])-1):
+                if j== 0:
+                    lst2.append(1)              
+                    value=save_data[i][j]+save_data[i][j+1]
+                    lst2.append(value)
+                else :
+                    val = save_data[i][j]+save_data[i][j+1]
+                    lst2.append(val)
+            lst2.append(1)    
+            save_data.append(lst2)  
+        for i in save_data:
+            listToStr = ' '.join([str(elem) for elem in i]) 
+            print(listToStr)
+                
 MyFunction(inp)
+
--- problem_forty_three.py
+++ problem_forty_three.py
@@ -6,13 +6,10 @@ def MyFunction(inp):
         lst2 =[]
         if i == 0:
             print(1)
-            print(1,1)    
-        elif i == 1:
-            print(1,i+1,1)
+            print(1,1)
             lst.append(1)
-            lst.append(i+1)
-            lst.append(1)
-        elif i>1:
+            lst.append(1)    
+        elif i>0:
             for j in range(len(lst)-1):
                 if j == 0:
                     lst2.append(1)
